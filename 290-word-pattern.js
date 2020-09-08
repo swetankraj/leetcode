cb448f09835f2673d0b0100b1c1cd281b4d85dfe
Swetank Raj <iamswetank@gmail.com>
2020-09-08 07:10:37 +0530

    updated using map() for better hashing

--- 290-word-pattern.js
+++ 290-word-pattern.js
@@ -17,3 +17,24 @@ var wordPattern = function(pattern, str) {
     }
     return true;
 };
+
+//! using Map() //
+/**
+ * @param {string} pattern
+ * @param {string} str
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, str) {
+    const str_arr = str.split(' ');
+    var dict = new Map(), dict2 = new Map();
+    if(str_arr.length !== pattern.length) return false
+    for(let i = 0; i<str_arr.length; i++){
+        if(!dict.has(pattern[i]) && !dict2.has(str_arr[i])) {
+            dict.set(pattern[i], str_arr[i]); 
+            dict2.set(str_arr[i], pattern[i]);
+        }
+        else if(dict.get(pattern[i]) === str_arr[i] && dict2.get(str_arr[i]) === pattern[i]) continue;
+        else return false;        
+    }
+    return true;
+};
