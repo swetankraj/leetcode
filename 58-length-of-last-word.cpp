d83f124b111719228b5a8d4f2c89352832873e69
Swetank Raj <iamswetank@gmail.com>
2020-09-15 22:24:20 +0530

    version 3

--- 58-length-of-last-word.cpp
+++ 58-length-of-last-word.cpp
@@ -37,3 +37,22 @@ public:
         return word.length();
     }
 };
+
+//VERSION 3
+class Solution
+{
+public:
+    int lengthOfLastWord(string s)
+    {
+        int n = s.size() - 1, s_len = 0;
+        while (n >= 0)
+        {
+            if (s[n] != ' ')
+                s_len++;
+            else if (s[n] == ' ' && s_len > 0)
+                return s_len;
+            n--;
+        }
+        return s_len;
+    }
+};
